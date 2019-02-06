754653df107bebcf3784a94b07f1cee860a00208
John M. Long <36753682+John-Michael-Long@users.noreply.github.com>
2019-02-05 23:01:29 -0600

    simplified blockchain class used for initial testing

---
+++ database/blockchain_simple.js
@@ -0,0 +1,56 @@
+const SHA256   = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+class Block {
+  constructor(index, timestamp, data, previousHash = ''){
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash(){
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, Date.now(), "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for(let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if(currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if(currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+module.exports.Block = Block;
+module.exports.Blockchain = Blockchain;
