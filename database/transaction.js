c8a2a3320549eb754b695f6886ccd1cd972dbbce
John M Long <36753682+John-Michael-Long@users.noreply.github.com>
2019-02-19 22:08:16 -0600

    clean up code

--- database/transaction.js
+++ database/transaction.js
@@ -16,19 +16,15 @@ class Transaction {
 
   signTransaction(signingKey) { 
     const hashTX = this.calculateHash();
-    const sig = signingKey.sign(hashTX, 'base64')  
+    const sig = signingKey.sign(hashTX, 'base64');  
 
-    // console.log('sig1: ', sig);
-
-    // Before we sign a transaction we need check that your public key === your fromAddress 
+    // must check that public key === fromAddress 
     // You can only spend coins from wallet you have the private key for. 
     // And because the private key is linked to the public key, public key must = fromAddress
     if(signingKey.getPublic('hex') !== this.fromAddress) {
       throw new Error('You cannot sign transactions for other wallets!');
     }
     this.signiture = sig.toDER('hex'); // here we save the signiture to the transaction .toDER is just a special format
-
-    // console.log('this.signiture: ', this.signiture);
   }
 
   // Verify transaction has been correctly signed
@@ -41,11 +37,6 @@ class Transaction {
 
     // here we create a new public-key object from the fromAddress because the fromAddress is a public-key
     const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
-
-    // console.log('isValid -> ec.keyFromPublic(this.fromAddress, hex) -> publicKey: ', publicKey)
-    // console.log('this.signiture: ', this.signiture);
-    // console.log('this.calculateHash: ', this.calculateHash());
-    // console.log('return publicKey.verify(this.calculateHash(), this.signiture): this returns true/false')
 
     // here we want to verify that the hash of this block has been signed by this.signiture
     return publicKey.verify(this.calculateHash(), this.signiture);
